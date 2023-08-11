160d96880db04a89e40f4739c3db8ae60dead71f
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2023-08-11 21:50:36 +0300

    Initail Commit.

---
+++ ToDOThings/ToDOThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOThingsApp.swift
+//  ToDOThings
+//
+//  Created by Ahmed Shaban on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDOThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
