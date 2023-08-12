ea1d638afab5345e82d8964c9e6446bbcdd04dda
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2023-08-12 22:00:36 +0300

    Card View Fix Positions

--- ToDOThings/Views/CardView.swift
+++ ToDOThings/Views/CardView.swift
@@ -13,67 +13,50 @@ struct CardView: View {
     
     var body: some View {
         ZStack {
-            // background
+            // Background Frame
             Rectangle()
                 .fill(Color.blue)
                 .cornerRadius(10)
-
             Image("cardMask")
-
             
-            // contents
-            VStack(spacing: 5) {
-                
-                // first row: symbol and menu dots
-                HStack {
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.white.opacity(0.4))
-                            .frame(width: 20, height: 25)
-                            .cornerRadius(5)
-                        Image(systemName: "iphone.gen1")
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    Image(systemName: "ellipsis")
-                        .foregroundColor(.white)
-                        .rotationEffect(.degrees(-90))
-                }
-                .padding(10)
-                .frame(maxHeight: .infinity, alignment: .top)
-
-                
-                Group {
+            // Content
+            HStack {
+                VStack(spacing: 10) {
+                    Group {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.white.opacity(0.4))
+                                .frame(width: 20, height: 25)
+                                .cornerRadius(5)
+                            Image(systemName: "iphone.gen1")
+                                .foregroundColor(.white)
+                        }
                         Text("2 Tasks")
                             .font(.caption2)
                             .foregroundColor(.white.opacity(0.6))
-                        
                         Text("Mobile App Design")
                             .font(.title3)
                             .fontWeight(.medium)
                             .foregroundColor(.white)
-                        
                         Text("I like to design mobile apps as It makes me happy.")
                             .font(.caption2)
                             .foregroundColor(.white.opacity(0.6))
-                            .padding(.bottom, 20)
-
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-
-
-                .padding(.leading, 5)
-                
-
-
-
-                
+                .padding(.leading, 10)
+                // Menu dots ...
+                VStack() {
+                    Image(systemName: "ellipsis")
+                        .foregroundColor(.white)
+                        .rotationEffect(.degrees(-90))
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding(.top, 15)
             }
-            
-            
         }
         .frame(width: 152, height: 190)
-
     }
 }
 
